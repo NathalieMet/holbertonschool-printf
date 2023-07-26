7049954469cd31efeaf2ad7220a59b9b13c7e9c9
ER1-53 <cremey.erwan@gmail.com>
2023-07-26 10:07:12 +0200

    correction betty

--- _printf.c
+++ _printf.c
@@ -15,45 +15,31 @@
 
 int _printf(const char *format, ...)
 {
-
 	va_list prints_type;
 	int i;
 	int j = 0;
 	int count_length = 0;
 
-
 	displayCharacter_t tableau_selection[] = {
-		{"s", print_funct_string},
-		{"c", print_funct_char},
-		{"d", print_funct_int},
-		{"i", print_funct_int}
-	};
-
+		{"s", print_funct_string}, {"c", print_funct_char},
+	{"d", print_funct_int}, {"i", print_funct_int}};
 
 	va_start(prints_type, format);
 
 	while (format != NULL && format[j] != '\0')
 	{
-
-
 		if (format[j] == '%')
 		{
 			if (format[j + 1] == '%')
-			{
-				count_length += _putchar(format[j]);
-				j += 2;
-			}
+			{	count_length += _putchar(format[j]);
+				j += 2; }
 			else
-			{
-				i = 0;
-
+			{ i = 0;
 				while (i < 4)
 				{
 					if (format[j + 1] == tableau_selection[i].specifierCharacter[0])
-					{
-						count_length += tableau_selection[i].f(prints_type);
-						j += 2;
-					}
+					{ count_length += tableau_selection[i].f(prints_type);
+						j += 2; }
 					i++;
 				}
 				count_length += _putchar(format[j]);
@@ -66,9 +52,6 @@ int _printf(const char *format, ...)
 			j++;
 		}
 	}
-
 	va_end(prints_type);
-
 	return (count_length);
-
 }
