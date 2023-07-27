280e54392621405dfb11328a16f4e4324e7560f1
ER1-53 <cremey.erwan@gmail.com>
2023-07-27 22:37:05 +0200

    debug j++ en trop

--- _printf.c
+++ _printf.c
@@ -13,43 +13,42 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list prints_type;
-    int i;
-    int j = 0;
-    int count_length = 0;
-    displayCharacter_t selection[] = {
-        {"s", print_funct_string}, {"c", print_funct_char},
-    {"d", print_funct_int}, {"i", print_funct_int}};
-    va_start(prints_type, format);
-    if (format == NULL)
-        return (-1);
-    while (format != NULL && format[j] != '\0')
-    {
-        if (format[j] == '%')
-        {
-            if (format[j + 1] == '%')
-            {   count_length += _putchar(format[j + 1]);
-                j += 2; }
-            else
-            { i = 0;
-                while (i < 4)
-                {
-                    if (format[j + 1] == selection[i].specifierCharacter[0])
-                    { count_length += selection[i].f(prints_type);
-                        j += 2;
-                        break; }
-                    i++;
-                }
-                /*if (selection[i].specifierCharacter == NULL && format[j + 1])*/
-                if (format[j] != '\0' && format[j] != '%')
-                {count_length += _putchar(format[j]);
-                j++;}
-                j++;
-            }
-        }
-        else
-        { count_length += _putchar(format[j]);
-            j++; }
-    }
-    va_end(prints_type);
-    return (count_length); }
+	va_list prints_type;
+	int i;
+	int j = 0;
+	int count_length = 0;
+	displayCharacter_t selection[] = {
+		{"s", print_funct_string}, {"c", print_funct_char},
+	{"d", print_funct_int}, {"i", print_funct_int}};
+	va_start(prints_type, format);
+	if (format == NULL)
+		return (-1);
+	while (format != NULL && format[j] != '\0')
+	{
+		if (format[j] == '%')
+		{
+			if (format[j + 1] == '%')
+			{   count_length += _putchar(format[j + 1]);
+				j += 2; }
+			else
+			{ i = 0;
+				while (i < 4)
+				{
+					if (format[j + 1] == selection[i].specifierCharacter[0])
+					{ count_length += selection[i].f(prints_type);
+						j += 2;
+						break; }
+					i++;
+				}
+				/*if (selection[i].specifierCharacter == NULL && format[j + 1])*/
+				if (format[j] != '\0' && format[j] != '%')
+				{count_length += _putchar(format[j]);}
+				j++;
+			}
+		}
+		else
+		{ count_length += _putchar(format[j]);
+			j++; }
+	}
+	va_end(prints_type);
+	return (count_length); }
